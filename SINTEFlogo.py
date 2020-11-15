8f04eb20cf0ae46609772f9ef627cd4552011b3f
Jon Vegard Venås <jonvegard89@yahoo.com>
2020-11-15 11:37:02 +0100

    generalize SINTEF logo script

--- SINTEFlogo.py
+++ SINTEFlogo.py
@@ -2,11 +2,11 @@
 from os.path import expanduser
 from paraview.simple import *
 # get active view
-renderView1 = GetActiveViewOrCreate('RenderView')
 
-home = expanduser("~")
-logo1 = Logo()
-logo1.Texture = CreateTexture(home+'/kode/paraUtils/sources/SINTEF_white.png')
-logo1Display = Show(logo1, renderView1, 'LogoSourceRepresentation')
-logo1Display.Position = [0.84, 0.0]
-logo1Display.Interactivity = 0
+def insertSINTEFlogo(renderview,color='white'):
+	home = expanduser("~")
+	logo1 = Logo()
+	logo1.Texture = CreateTexture(home+'/kode/paraUtils/sources/SINTEF_'+color+'.png')
+	logo1Display = Show(logo1, renderview, 'LogoSourceRepresentation')
+	logo1Display.Position = [0.84, 0.0]
+	logo1Display.Interactivity = 0
