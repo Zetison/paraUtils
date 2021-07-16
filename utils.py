0a573b192f3e870854d2de2bb2adea35f2277415
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-07-16 16:36:34 +0200

    changes

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#from pyproj import Proj
+from pyproj import Proj
 from os.path import expanduser
 from paraview.simple import *
 import numpy as np
@@ -9,13 +9,13 @@ def prsLatLonStr(dmsStr):
 def dms2dd(dms):
     return float(dms[0]) + float(dms[1])/60 + float(dms[2])/3600
 
-#defaultProj = "+proj=utm +zone=33K, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-#def latLonUTM(lat,lon,z,projString=defaultProj):
-#    myProj = Proj(projString)
-#    declat = dms2dd(prsLatLonStr(lat))
-#    declon = dms2dd(prsLatLonStr(lon))
-#    lonUTM, latUTM = myProj(declon,declat)
-#    return np.array([lonUTM,latUTM,z*0.3048])
+defaultProj = "+proj=utm +zone=33K, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+def latLonUTM(lat,lon,z,projString=defaultProj):
+    myProj = Proj(projString)
+    declat = dms2dd(prsLatLonStr(lat))
+    declon = dms2dd(prsLatLonStr(lon))
+    lonUTM, latUTM = myProj(declon,declat)
+    return np.array([lonUTM,latUTM,z*0.3048])
 
 def annotateTimeStep(obj,renderview,location='UpperLeftCorner',SAVE_HIST=1,color=[0.0, 0.0, 0.0]):
     pythonAnnotation = PythonAnnotation(registrationName='Time annotation', Input=obj)
@@ -59,7 +59,7 @@ def saveScreenShot(renderView,name,saveScreenShots=True,viewSize=[1920,1080],use
 def saveAnimation(renderViewOrLayout,name,noSteps,makeVideo=True,viewSize=[1920,1080],frameRate=15,animStart=0,saveAllViews=0,separatorWidth=0):
     if makeVideo:
         if not saveAllViews:
-            renderView.ViewSize = viewSize
+            renderViewOrLayout.ViewSize = viewSize
 
         animationScene1 = GetAnimationScene()
         SaveAnimation(name+'.ogv', renderViewOrLayout,SaveAllViews=saveAllViews,SeparatorWidth=separatorWidth,
